29bbee357d012f1feb25cda720c3badc28333320
OnloadMyBrain <stancode203@gmail.com>
2018-10-14 15:54:46 +0900

    -

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ const ejs = require('ejs');
 http.createServer(function(request, response){
   fs.readFile('ejs.ejs', 'utf-8', function(error,data){
     response.writeHead(200, {'Content-type':'text/html'});
-    response.end(ejs.render(data));
+    response.end(ejs.render(data,{
+      "name":"son",
+      "address" : "Jamsil-dong"
+    }));
   });
 }).listen(3000, function(){
   console.log('Server running...');
