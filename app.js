e0947a11cde890384bd359da6e7f4f14264b2fc7
OnloadMyBrain <stancode203@gmail.com>
2018-10-14 15:10:07 +0900

    get,post

--- app.js
+++ app.js
@@ -7,21 +7,32 @@ const url = require('url');
 http.createServer(function(request,response){
   // 페이지 이동
 
-  var pathname = url.parse(request.url).pathname;
+  // var pathname = url.parse(request.url).pathname;
   // ohterpage 구분
 
-  if(pathname == '/'){
-    // index.html
-    fs.readFile('index.html', function(error,data){
-      response.writeHead(200, {'content-Type':'text/html'});
-      response.end(data);
-    })
-  }else if(pathname == '/otherpage'){
-    fs.readFile('otherpage.html', function(error,data){
-      response.writeHead(200, {'Content-Type':'text/html'});
-      response.end(data);
-    })
-  }
-}).listen(3000, function(){
-  console.log('Server runinig...');
+//   if(pathname == '/'){
+//     // index.html
+//     fs.readFile('index.html', function(error,data){
+//       response.writeHead(200, {'Content-Type':'text/html'});
+//       response.end(data);
+//     })
+//   }else if(pathname == '/otherpage'){
+//     fs.readFile('otherpage.html', function(error,data){
+//       response.writeHead(200, {'Content-Type':'text/html'});
+//       response.end(data);
+//     })
+//   }
+// }).listen(3000, function(){
+//   console.log('Server running...');
+    if(request.method == 'GET'){
+      // GET
+      var query = url.parse(request.url, ture).query;
+
+      response.writeHead(200, {'Content-type':'text/html'});
+      response.end('<h1>'.JSON.stringfy(query) + '</h1>');
+    }else if(request.mothod == 'POST'){
+      // POST
+    }
+  }).listen(3000, function(){
+  console.log('Server running...');
 });
