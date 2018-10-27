b620c6c1dcc26fa12ef545940cd45a85f0c79211
overdiv <stancode203@gmail.com>
2018-10-27 18:45:44 +0900

    create express package

---
+++ express-run/app.js
@@ -0,0 +1,46 @@
+//module
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+//router
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+//node setting
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); //pug JADE nunjucks.js
+
+//middleware
+app.use(logger('dev'));
+app.use(express.json()); //bodyparser. 화면에 뿌려진 json 값을 가져오게하는 미들웨어.
+app.use(express.urlencoded({ extended: false })); // get  ?name=abc&address=adsdf => json
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));  //static file load.
+
+//url 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
