17f910616ef20319502dd703c54eb745866239d4
overdiv <stancode203@gmail.com>
2018-11-03 16:14:49 +0900

    create sequelize router

---
+++ sequelize-run/models/comment.js
@@ -0,0 +1,15 @@
+module.exports=(sequelize, DataTypes) => {
+    return sequelize.define('comment', {   
+		comment:{
+			type:DataTypes.TEXT,
+			allowNull:false,
+		},
+		created_at:{
+			type:DataTypes.DATE,
+			allowNull:false,
+			defaultValue:sequelize.literal('now()'),
+		}
+    },{
+        timestamps:false,
+    })
+}
---
+++ sequelize-run/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var User = require('../models').User;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // DB 모든 user 정보를 다 가져옴. sequelize.pug 페이지를 띄워야함
+  // res.render('index', { title: 'Express' });
+  User.findAll()
+  .then((users)=> {
+    res.render('sequelize', {users});
+  })
+  .catch((err)=> {
+    console.error(err);
+    next(err);
+  });
+});
+
+module.exports = router;
